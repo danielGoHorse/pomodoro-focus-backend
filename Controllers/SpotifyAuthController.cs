32184f4e49729c330b03da0c467cdab212e0c311
danielGoHorse <profe.gotardo@gmail.com>
2025-03-23 20:46:02 -0300

    fix: ajuste de endpoint

--- Controllers/SpotifyAuthController.cs
+++ Controllers/SpotifyAuthController.cs
@@ -81,3 +81,11 @@ public class SpotifyAuthController : ControllerBase
     }
 }
 
+public class SpotifyTokenResponse
+{
+    public string access_token { get; set; }
+    public string token_type { get; set; }
+    public int expires_in { get; set; }
+    public string refresh_token { get; set; }
+    public string scope { get; set; }
+}
